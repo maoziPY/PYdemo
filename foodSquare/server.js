e52b221ccd6d5b42353b22be321474b25348e4ca
wupeiyue <wupeiyue@wps.cn>
2017-03-24 16:03:14 +0800

    测试1

--- foodSquare/server.js
+++ foodSquare/server.js
@@ -10,7 +10,8 @@ var server = app.listen(8081, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log(host);
+	console.log(host2);
+	console.log(port);
 
 	console.log('应用实例， 访问地址为http://%s:%s', host , port);
 });
