6dbbb1a5d55a787a188c42960f105833af04c212
wupeiyue <wupeiyue@wps.cn>
2017-03-27 17:38:02 +0800

    登录模块

--- foodSquare/server.js
+++ foodSquare/server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
 	res.sendFile( __dirname + '/public/views/index/' + 'index.html');
 });
 
+app.get('/login', function(req, res) {
+	res.sendFile( __dirname + '/public/views/login/' + 'index.html');
+});
+
 app.use(express.static('public'));
 
 var server = app.listen(8081, function() {
