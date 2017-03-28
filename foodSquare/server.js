d4c41640f4df7e1b9b19bbe8db4c9b6aef535240
wupeiyue <wupeiyue@wps.cn>
2017-03-28 18:18:27 +0800

    admin

--- foodSquare/server.js
+++ foodSquare/server.js
@@ -9,6 +9,10 @@ app.get('/login', function(req, res) {
 	res.sendFile( __dirname + '/public/views/login/' + 'index.html');
 });
 
+app.get('/admin', function(req, res) {
+	res.sendFile( __dirname + '/public/views/admin/' + 'index.html');
+});
+
 app.use(express.static('public'));
 
 var server = app.listen(8081, function() {
