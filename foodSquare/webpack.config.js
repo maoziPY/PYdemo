36ee4374be0c9fd204ab5311b1aa2689e24a2105
maoziPY <550632474@qq.com>
2017-07-27 17:46:42 +0800

    构建调整

--- foodSquare/webpack.config.js
+++ foodSquare/webpack.config.js
@@ -1,18 +1,25 @@
 var path = require('path');
 
+var buildPath = 'Login';
+
 module.exports = {
     entry: './public/javascript/Login/main.js',
     output: {
-        path: path.resolve(__dirname, './public/dist'),
+        path: path.resolve(__dirname, './public/dist/'+buildPath),
         filename: 'bundle.js',
     },
     module: {
         rules: [{
             test: /\.(js|jsx)$/, //一个匹配loaders所处理的文件的拓展名的正则表达式，这里用来匹配js和jsx文件（必须）
-            exclude: /node_modules/,//屏蔽不需要处理的文件（文件夹）（可选）
+            include: [
+                path.resolve(__dirname, './public/javascript/'+buildPath)
+            ],
             use: 'babel-loader'
         }, {
             test: /\.css$/,
+            include: [
+                path.resolve(__dirname, './public/resources/'+buildPath)
+            ],
             use: [
                 'style-loader',
                 'css-loader'
